c34307f5cb90aa066894ab5f3d0eea8ddb395b4a
Oscar <oscarrobertrodriguez@gmail.com>
2022-06-28 19:20:26 -0500

    fix document.js

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,48 +1,38 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+import stylisRTLPlugin from 'stylis-plugin-rtl'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+    const page = renderPage(
+      (App) => (props) =>
+        sheet.collectStyles(
+          <StyleSheetManager stylisPlugins={[stylisRTLPlugin]}>
+            <App {...props} />
+          </StyleSheetManager>
+        )
+    )
 
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: [
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>,
-        ],
-      }
-    } finally {
-      sheet.seal()
-    }
+    const styleTags = sheet.getStyleElement()
+
+    return { ...page, styleTags }
   }
-
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500&display=swap" rel="stylesheet"></link>
-        </Head>
+          {this.props.styleTags}
+          </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
